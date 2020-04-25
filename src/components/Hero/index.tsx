b3c551edadce50a5b424b8149f0ac1d29fc38769
Sthéffane Nunes <stheffane.tr@gmail.com>
2020-04-25 12:16:55 -0300

    Inicializando projeto/ Base pronta

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import { TILE_SIZE, HEAD_OFSET } from '../../setings/constants'
+
+import './styles.css';
+
+import useHeroMoviment from '../../hooks/useHeroMoviment'
+
+const Hero = () => {
+
+    const { positionX, positionY, direction } = useHeroMoviment({ x: 1, y: 2 })
+
+    return (
+        <div
+            style={{
+                position: 'absolute',
+                bottom: TILE_SIZE * positionY,
+                left: TILE_SIZE * positionX,
+                width: TILE_SIZE,
+                height: TILE_SIZE + HEAD_OFSET,
+                backgroundImage: 'url(./assets/HERO.png)',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: `0px -${TILE_SIZE - HEAD_OFSET}px`,
+                animation: 'hero-animation .7s steps(4) infinite',
+                transform: `scaleX(${direction === 'right' ? 1 : -1})`,
+                zIndex: 1,
+
+            }}
+        />
+    );
+}
+
+export default Hero
---
+++ src/components/MiniDemon/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import { TILE_SIZE, HEAD_OFSET } from '../../setings/constants'
+
+import useEnemyMoviment from '../../hooks/useEnemyMoviment'
+
+import './styles.css';
+
+const MiniDemon = () => {
+    const { positionX, positionY, direction } = useEnemyMoviment({ x: 10, y: 5 })
+
+    return (
+        <div
+            style={{
+                position: 'absolute',
+                bottom: TILE_SIZE * positionY,
+                left: TILE_SIZE * positionX,
+                width: TILE_SIZE,
+                height: TILE_SIZE + HEAD_OFSET,
+                backgroundImage: 'url(./assets/MINI-DEMON.png)',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: `0px -${TILE_SIZE - HEAD_OFSET}px`,
+                animation: 'miniDemon-animation .6s steps(4) infinite',
+                transform: `scaleX(${direction === 'right' ? 1 : -1})`,
+            }}
+        />
+    );
+}
+
+export default MiniDemon
